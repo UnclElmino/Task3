298b290a171be553f64c7299865f253698e27545
UnclElmino <msiemilio@myseneca.ca>
2025-09-29 03:28:10 -0400

    initial push

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { lcm } = require('mathjs');
+
+const app = express();
+
+const routePath = '/emailio_imam09_gmail_com';
+
+app.get(routePath, (req, res) => {
+    res.set('Content-Type', 'text/plain; charset=utf-8');
+
+    const x = Number(req.query.x);
+    const y = Number(req.query.y);
+    try {
+        const result = lcm(x, y);
+        res.send(String(result));
+    } catch {
+        res.send('NaN');
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
